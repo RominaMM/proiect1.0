be038b17d7b9679062fce3c6a84c6bc4e9395254
romina <rominamihaila@gmail.com>
2020-01-08 22:07:33 +0200

    Project1

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Exercitiu 1: Hello");
+        System.out.println("Romina");
+
+        int x=2+3;
+        System.out.println("Exercitiu 2: 2+3=" + x);
+
+        int y=10/2;
+        System.out.println("Exercitiu 3: 10/2=" + y);
+
+        System.out.println("Exercitiu 4:");
+        int a= -5+8*6;
+        System.out.println("-5+8*6=" + a);
+        int b= (55+9)%9;
+        System.out.println("(55+9)%9=" +b);
+        int c=  20+ -3*5/8;
+        System.out.println( "20+ -3*5/8=" + c);
+        int d= 5+15 / 3*2 - 8 % 3;
+        System.out.println("5+15 / 3*2 - 8 % 3=" + d);
+
+
+
+
+
+    }
+
+
+
+
+}

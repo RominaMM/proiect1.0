5643084c9a629cd351112fca61d9e4a79406c782
romina <rominamihaila@gmail.com>
2020-01-12 22:23:13 +0200

    TemaLab2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,50 +2,107 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+
+        printName();
+        ex2();
+        ex3();
+        ex4a();
+        ex4b();
+        ex4c();
+        ex4d();
+
+
+
+    }
+
+    public static void printName() {
         System.out.println("Exercitiu 1: Hello \nRomina");
+    }
 
-
-        // Exercitiu 2:
-        float x = 10f;
-        float y = 3f;
+    public static void ex2(){
+       float x = 10f;
+        float y= 3f;
         float sum = x + y;
         System.out.println("Exercitiu 2: " + sum);
-
-        // Exercitiu 3:
+    }
+    public static void ex3(){
+        float x = 10f;
+        float y= 3f;
         float div = x / y;
-
         System.out.println("Exercitiu 3: " + div);
-
-        //Exercitiu 4:
+    }
+    public static void ex4a(){
         int a = -5 + 8 * 6;
         System.out.println("Exercitiu 4:  \na. " + a);
+    }
+    public static void ex4b(){
+        int b = 55+9;
+        float d = 0.09f ;
+        double e = b*d ;
+        System.out.println("b. " + e );
+    }
 
-         int b = 55+9;
-         float d = 0.09f ;
-         double e = b*d ;
-         System.out.println("b. " + e );
+    public static void ex4c(){
+        int k = 20;
+        float j = -3*5;
+        int m = 8;
+        float r = k + j / m;
+        System.out.println("c. " + r ) ;
+    }
 
-         int k = 20;
-         float j = -3*5;
-         int m = 8;
-         float r = k + j / m;
-        System.out.println("c. " + r );
-
+    public static void ex4d(){
         int v = 5;
         float w = 15/3f*2;
         int p = 8;
         float u= 0.03f;
         float t = v + w - p * u;
         System.out.println("d. " + t );
-
-
-
-
-
     }
 
 
-
-
-
 }
+
+        //Exercitiu 2:
+       //float x = 10f;
+        //float y = 3f;
+     //float sum = x + y;
+       //System.out.println("Exercitiu 2: " + sum);
+
+        // Exercitiu 3:
+       // float div = x / y;
+
+       // System.out.println("Exercitiu 3: " + div);
+
+        //Exercitiu 4:
+       // int a = -5 + 8 * 6;
+       // System.out.println("Exercitiu 4:  \na. " + a);
+
+         //int b = 55+9;
+        // float d = 0.09f ;
+         //double e = b*d ;
+        // System.out.println("b. " + e );
+
+        // int k = 20;
+        // float j = -3*5;
+        // int m = 8;
+       //  float r = k + j / m;
+       // System.out.println("c. " + r );
+
+       // int v = 5;
+      //  float w = 15/3f*2;
+      //  int p = 8;
+      //  float u= 0.03f;
+      //  float t = v + w - p * u;
+      //  System.out.println("d. " + t );
+
+
+
+
+
+   // }
+
+
+
+
+
+

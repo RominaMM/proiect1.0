ca739229ee03142ef54684226879821064c21e64
romina <rominamihaila@gmail.com>
2020-02-12 23:54:38 +0200

    TemaLab5

---
+++ src/Lab5/Calculator/Main.java
@@ -0,0 +1,21 @@
+package Lab5.Calculator;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = { 8, -2, 6, 19};
+        LogicalOP op = new LogicalOP();
+
+        System.out.println(op.secondSmallestnumb(array));
+
+
+
+
+
+
+    }
+}
+
+
